964d9f47f796273b2af89d24246cdc1be652cecf
RayM2 <rayhan.mohammed15@gmail.com>
2024-12-14 22:57:28 -0500

    Fix issues in dashApp.py and update requirements

--- dashApp.py
+++ dashApp.py
@@ -195,4 +195,4 @@ def predict_value(n_clicks, input_values):
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True)
